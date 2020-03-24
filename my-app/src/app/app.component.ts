f7666876014bc3f22a5c79ff14c41e547a5e3695
SandraGro <sandra.guerreroflores@gmail.com>
2020-03-23 23:36:50 -0600

    Application base

---
+++ my-app/src/app/app.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = ""
+  showEditField = true;
+  list = []
+  constructor(){
+    this.title = 'To-Do list';
+
+    this.list = [
+      {
+        title: 'First item',
+        completed: true
+      },
+      {
+        title: 'Second item',
+        completed: false
+      },
+      {
+        title: 'Tßhird item',
+        completed: false
+      }];
+  }
+
+  addTodo(){
+    console.log('add');
+  };
+
+  updateTodo(item, index){
+    console.log('add');
+  };
+
+  removeTodo(item, index){
+    console.log('add');
+  };
+
+  toggleState(item){
+    console.log('toggle');
+
+  };
+  
+  }
+
